c01a0a5fb9a442536d51940afd0f1484fe19af9a
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-08-24 18:46:58 +0530

    📌How to create a Servlet Filter
    📌Create a Java class and implement javax.servlet.Filter object 👇
    
    public class Filter1 implements Filter{
    
        @Override
        public void init(FilterConfig filterConfig) throws ServletException {
            System.out.println("Filter Init");
        }
    
        @Override
        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
            System.out.println("Filter1");
        }
    
        @Override
        public void destroy() {
            System.out.println("Filter Destroy");
        }
    
    }

---
+++ src/java/model/Filter1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author isharaLakshitha
+ */
+public class Filter1 implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter Init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Filter1");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Filter Destroy");
+    }
+    
+}
--- web/index.jsp
+++ web/index.jsp
@@ -4,8 +4,6 @@
     Author     : isharaLakshitha
 --%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-<%@taglib prefix="core" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@taglib prefix="c" uri="/WEB-INF/tlds/ctl" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -13,25 +11,6 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Custom Tag Libraries (CTL)</h1>
-
-        <c:user id="1">
-            <c:name>Sahan</c:name>
-        </c:user>
-
-        <c:user id="2">
-            <c:name>Kasun</c:name>
-        </c:user> 
-
-        <c:user id="4">
-            <c:name>Ishara</c:name>
-        </c:user>
-
-
-
-        <h2>${applicationScope.user1.name}</h2>
-        <h2>${applicationScope.user2.name}</h2>
-        <h2>${applicationScope.user4.name}</h2>
-
+        <h1>Servlet Filters</h1>
     </body>
 </html>
